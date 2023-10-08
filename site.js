e31fdccbfc53bba20ed52e575ee776ab48d89c7f
leo-esnault <114683941+leo-esnault@users.noreply.github.com>
2023-10-08 22:23:41 +0200

    Add files via upload

---
+++ site.js
@@ -0,0 +1,44 @@
+window.addEventListener("scroll", function () {
+    var header = document.querySelector(".header");
+    if (window.scrollY >= 150) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-deroulant");
+    const navigation = document.querySelector(".navigation");
+
+    menuToggle.addEventListener("click", function () {
+        navigation.classList.toggle("active");
+    });
+});
+
+$(document).ready(function(){
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: $('.suivant'),
+        prevArrow: $('.precedent'),
+        responsive: [
+            {
+              breakpoint: 1300,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 800,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+      });
+});
